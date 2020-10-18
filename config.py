ee7211ec813d082d6c9b7b7459228f12cc25a82c
chencheng <18895372792@163.com>
2020-10-18 19:52:43 +0800

    update

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ class get_args:
         # training config
         self.optim = 'adam'
         self.lr = 1e-5
-        self.epoches = 40
+        self.epoches = 100
         self.lr_patience = 2  # if get patience time continuous bad mean loss, then change the lr
         self.val_interval = 4
-        self.start_epoch = 27
+        self.start_epoch = 41
         self.GPU_nums = 2
 
         # dataset config
@@ -20,7 +20,6 @@ class get_args:
         self.val_set = 'val2017'
         self.num_workers = 12
         self.batch_size = 9
-        self.num_epochs = 40
         # mean and std in RGB order, actually this part should remain unchanged as long as your dataset is similar to coco.
         self.mean = [0.485, 0.456, 0.406]
         self.std = [0.229, 0.224, 0.225]
@@ -32,14 +31,18 @@ class get_args:
         self.load_weights = None
         self.save_path = 'checkpoints/'
         self.load_from = True
-        self.load_from_path = 'checkpoints/coco/epoch_27_loss_1.23753.pth'
+        self.load_from_path = '/home/workspace/chencheng/Learning/ObjectDetection/retinanet.pytorch/checkpoints/coco/epoch_40_loss_0.52891.pth'
 
         # inference config
+        self.use_soft_nms = False
         self.nms_thresh = 0.3
         self.score_thresh = 0.25
         self.results_save_path = 'results/'
-        self.inference_resize_wh = (768, 640)
+        self.inference_resize_wh = (1024, 768)
         self.nms_with_cls = False
+
+        # evaluate config
+        self.eval_json_save_path = '/home/workspace/chencheng/Learning/ObjectDetection/retinanet.pytorch/results/eval'
 
         self.classes = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', \
                         'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', \
--- predict.py
+++ predict.py
@@ -1,7 +1,7 @@
 import os
 import cv2
-import random
 import numpy as np
+from collections import OrderedDict
 
 import torch
 import torch.nn as nn
@@ -9,83 +9,47 @@ import matplotlib.patches as patches
 import matplotlib.pyplot as plt
 
 from model.retinanet import get_model
-from model.utils import BBoxTransform, ClipBoxes, nms_with_class, nms_without_class
-
-model_path = '/home/workspace/chencheng/Learning/ObjectDetection/retinanet.pytorch/checkpoints/mchar/epoch_11_loss_0.57797.pth'
-retinanet = get_model(num_classes=10, pretrained=False)
-retinanet.load_state_dict(torch.load(model_path))
-retinanet.eval()
-
-classes = {0: '1', 1:'2', 2:'3', 3:'4', 4:'5', 5:'6', 6:'7', 7:'8', 8:'9', 9:'0'}
+from script.utils import predict_single_imgs
+from config import get_args
 
 
-def process_imgs(model, img_path, classes, score_thresh=0.3, \
-                iou_thresh=0.3, nms_with_cls=True, \
-                resize_wh=(768, 640), save_path='results/' ,
-                mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
-
-    regressBoxes = BBoxTransform()
-    clipBoxes = ClipBoxes()
-
-    img = cv2.imread(img_path)[..., ::-1]
-    normalized_img = (img / 255 - mean) / std
-    ori_h, ori_w = normalized_img.shape[:2]
-
-    scale = resize_wh[1] / ori_h
-    if scale * resize_wh[0] > ori_w:
-        scale = resize_wh[0] / ori_w
-    new_h, new_w = int(scale * ori_h), int(scale * ori_w)
-    img_resized = cv2.resize(normalized_img, (new_w, new_h))
-
-    img_paded = np.zeros([resize_wh[1], resize_wh[0], 3])
-    img_paded[:new_h, :new_w, :] = img_resized
-
-    img_input = torch.from_numpy(img_paded).permute(2, 0, 1).unsqueeze(dim=0).float()
-    features, regression, classification, anchors = model(img_input)
-
-    # add predict offset
-    trans_anchors = regressBoxes(anchors, regression)
-    trans_anchors = clipBoxes(trans_anchors, img_input)
-
-    scores = torch.max(classification, dim=2, keepdim=True)[0]  # 计算anchor的最高得分
-    scores_over_thresh = (scores > score_thresh)[:, :, 0]
-
-    top_cls = classification[0, scores_over_thresh[0, :], ...].permute(1, 0)  # get classes over thresh
-    top_bbox = trans_anchors[0, scores_over_thresh[0, :], ...]  # get bbox over thresh
-    top_scores = scores[0, scores_over_thresh[0, :], ...]  # get scores
-    scores_, classes_ = top_cls.max(dim=0)  # score and label_id for every bbox
-
-    if nms_with_cls:
-        anchors_nms_idx = nms_with_class(top_bbox, top_scores[:, 0], classes_, iou_threshold=iou_thresh)
-    else:
-        anchors_nms_idx = nms_without_class(top_bbox, top_scores[:, 0], iou_threshold=iou_thresh)
-    
-    final_bbox = top_bbox[anchors_nms_idx]
-    final_cls = classes_[anchors_nms_idx]
-
-    # reduct to ori img and detach
-    if torch.cuda.is_available():
-        map2ori_bbox = (final_bbox / scale).cpu().detach().numpy()
-        final_cls = final_cls.cpu().detach().numpy()
-    else:
-        map2ori_bbox = (final_bbox / scale).detach().numpy()
-        final_cls = final_cls.detach().numpy()
-    
+def process_imgs(model, img_path, cfg):
+    img = cv2.imread(img_path)
+    colors = [(np.random.rand(), np.random.rand(), np.random.rand()) for _ in range(cfg.num_classes)]
+    bbox_results, classes_results, scores_results = predict_single_imgs(model, img_path, cfg)
     # imshow
-    plt.imshow(img)
-    for idx, rect in enumerate(map2ori_bbox):
+    plt.imshow(img[..., ::-1])
+    for idx, rect in enumerate(bbox_results):
         x1 = rect[0]
         y1 = rect[1]
         x2 = rect[2]
         y2 = rect[3]
         w = x2 - x1
         h = y2 - y1
-        plt.gca().add_patch(patches.Rectangle((x1, y1), w, h, color=(1, 0, 0), fill=False))
-        plt.text(x1, y1, classes[final_cls[idx]], color=(0, 1, 0))
-    plt.savefig(save_path + 'demo.jpg', dpi=120)
+        cls_id = classes_results[idx]
+        plt.gca().add_patch(patches.Rectangle((x1, y1), w, h, color=colors[cls_id], fill=False))
+        plt.text(x1, y1, cfg.classes[cls_id], bbox=dict(boxstyle='square,pad=0.2',fc=colors[cls_id], lw=0, alpha=0.5))
+    # plt.savefig(save_path + 'demo.jpg', dpi=120)
+    plt.show()
 
 
 if __name__ == '__main__':
-    img_path = '/home/workspace/chencheng/libc++/efficientdet/datasets/char_recognize/mchar_val/000026.png'
-    process_imgs(retinanet, img_path, classes)
+    cfg = get_args()
+    retinanet = get_model(num_classes=cfg.num_classes, pretrained=False)
+    state_dict = torch.load(cfg.load_from_path)
+    
+    if cfg.GPU_nums > 1:
+        new_state_dict = OrderedDict()
+        for k, v in state_dict.items():
+            name = k[7:]   # remove `module.`
+            new_state_dict[name] = v
+        retinanet.load_state_dict(new_state_dict)
+    else:
+        retinanet.load_state_dict(state_dict)
+
+    retinanet = retinanet.cuda()
+    retinanet.eval()
+
+    img_path = '/home/workspace/chencheng/Learning/ObjectDetection/Datasets/CoCodataset/train2017/000000580908.jpg'
+    process_imgs(retinanet, img_path, cfg)
 
